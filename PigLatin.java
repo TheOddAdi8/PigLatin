fa0cfb948bc8a264551d4f9237029ab3e51dacd8
Adi Duggal <duggaadi25@7hills.org>
2023-09-08 13:19:50 -0400

    for loops and stuff will finish later

--- PigLatin.java
+++ PigLatin.java
@@ -4,18 +4,29 @@ import java.util.Scanner;
 
 public class PigLatin {
 
+    char[] consonants = {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'};
+    char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+
     public static void main(String[] args) {
 
-        String word = "";
+        Scanner pigBot = new Scanner(System.in);
 
-        int len = word.length()
+        String word = pigBot.nextLine();
+
+        word = word.toLowerCase();
+
+        int len = word.length();
 
         for (int i = 0; i < len; i++) {
-            if (word[i].equals("a")) {
-                System.out.println("Found the letter a");
+            for(int j = 0; j < consonants; j++) {}
+                if (word.charAt(i) == 'a') {
+                    System.out.println("Found the letter 'a' at position " + i);
+                }
+                word.substring(i, i + 1); //up to but not including i + 1
             }
-            word.substring(i, i + 1); //up to but not including i + 1
         }
+
+        pigBot.close();
 
     }
 }
