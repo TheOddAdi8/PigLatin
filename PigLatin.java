ebc5613e52c46913e3999cdbc1d31491f0650ae5
Adi Duggal <duggaadi25@7hills.org>
2023-09-12 12:18:43 -0400

    Made it work, partially restarted

--- PigLatin.java
+++ PigLatin.java
@@ -4,40 +4,25 @@ import java.util.Scanner;
 
 public class PigLatin {
 
-    char[] consonants = {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'};
-    char[] vowels = {'a', 'e', 'i', 'o', 'u'};
-
     public static void main(String[] args) {
 
         Scanner pigBot = new Scanner(System.in);
 
-        String word = pigBot.nextLine();
+        String phrase;
+        int len;
+        String temp;
+        String constant = "ay";
 
-        word = word.toLowerCase();
+        System.out.print("Enter a word: ");
 
-        int len = word.length();
-
-        //int consonantsLen = 21;
-        //int vowelsLen = 5;
-
-        /*for (int i = 0; i < len; i++) {
-            for(int j = 0; j < consonantsLen; j++) {}
-                if (word.charAt(i) == 'a') {
-                    System.out.println("Found the letter 'a' at position " + i);
-                }
-                word.substring(i, i + 1); //up to but not including i + 1
-            }
-        }*/
-
-        char letter;
-
-        for (int k = 0; k < len; k++) {
-            letter = word.charAt(k);
-            for (int l = 0; l < 21; l++) {
-                
-            }
-            word.substring(0, k + 1);
-
+        phrase = pigBot.nextLine().toLowerCase();
+        len = phrase.length();
+        temp = phrase.substring(0, 1);
+        if (temp.equals("a") || temp.equals("e") || temp.equals("i") || temp.equals("o") || temp.equals("u")) {
+            System.out.println(phrase + "w" + constant);
+        }
+        else {
+            System.out.println(phrase.substring(1, len) + temp + constant);
         }
 
         pigBot.close();
